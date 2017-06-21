996a18163d423ee863e4a05318d3d034bb3637a4
yanshijing <yanshijing@comjia.com>
2017-06-21 17:04:09 +0800

    目录结构优化

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,15 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
 
+//基本路径
+var pcSrcPath = "./src/pc/";  //PC版源码路径
+var pcReleasePath = "./dist/pc/";  //PC版发布文件路径
+var mSrcPath = "./src/m/";  //M版源码路径
+var mRrleasePath = "./dist/m/";  //M版发布文件路径
+
+
+gulp.task("less",function(){
+    return gulp.src(pcSrcPath + "less/*.less")
+        .pipe(less())
+        .pipe(gulp.dest(pcReleasePath + "css"));
+});
